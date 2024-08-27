e9b2f475409fb262b3d2e5d5210c4cc1c2e2487d
eduardo98990988 <i2219802@continental.edu.pe>
2024-08-27 15:24:34 -0500

    funciona

--- src/app/Pasarela/pasarela-module/pages/pag-pago/pag-pago.component.ts
+++ src/app/Pasarela/pasarela-module/pages/pag-pago/pag-pago.component.ts
@@ -1,17 +1,28 @@
+
+
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { environment } from '../../environments/environment'; // Ajusta la ruta si es necesario
-import { loadStripe, Stripe } from '@stripe/stripe-js';
+import { environment } from '../../enviorements/enviorements';
+
+declare global {
+  interface Window {
+    Stripe?: any;
+  }
+}
+
+
 
 @Component({
   selector: 'app-pag-pago',
   templateUrl: './pag-pago.component.html',
   styleUrls: ['./pag-pago.component.css']
 })
+
 export class PagPagoComponent implements OnInit {
+ 
+
   paymentForm: FormGroup;
   showInfo: boolean = false;
-  private stripe: Stripe | null = null;
 
   constructor(private fb: FormBuilder) {
     this.paymentForm = this.fb.group({
@@ -21,42 +32,15 @@ export class PagPagoComponent implements OnInit {
       cvv: ['', [Validators.required, Validators.pattern(/^\d{3}$/)]]
     });
   }
-
-  async ngOnInit(): Promise<void> {
-    try {
-      this.stripe = await loadStripe(environment.stripePublicKey); 
-    } catch (error) {
-      console.error('Error al cargar Stripe:', error);
-    }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
-  async processPayment(): Promise<void> {
+  processPayment(): void {
     if (this.paymentForm.valid) {
       const paymentDetails = this.paymentForm.value;
       console.log('Processing payment with details:', paymentDetails);
-
-      if (this.stripe) {
-        try {
-          const { token, error } = await this.stripe.createToken('card', {
-            number: paymentDetails.cardNumber.replace(/-/g, ''),
-            exp_month: parseInt(paymentDetails.expiryDate.split('/')[0], 10),
-            exp_year: parseInt(paymentDetails.expiryDate.split('/')[1], 10),
-            cvc: paymentDetails.cvv,
-            name: paymentDetails.cardName
-          });
-
-          if (error) {
-            console.error('Error creating token:', error);
-          } else if (token) {
-            console.log('Token created:', token);
-            // Aquí deberías integrar con tu API de pagos real utilizando el token
-          }
-        } catch (stripeError) {
-          console.error('Error during payment process:', stripeError);
-        }
-      } else {
-        console.error('Stripe.js not loaded');
-      }
+      // Aquí deberías integrar con tu API de pagos real.
     } else {
       console.log('Formulario inválido');
     }
